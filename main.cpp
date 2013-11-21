a3e11117c582db00b762d97ac72be8990a8ab73f
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2013-11-21 15:32:51 -0600

    matrixMontante has all data

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@
 #include <iostream>
 using namespace std;
 
-int datos, matrixSums[10][10] , grado, sumPotX[10], sumPotxY[10], x[50],y[50], sumY;
-int potX[50];
+int datos, matrixSums[50][50] , grado, sumPotX[20], sumPotxY[20], x[50],y[50], sumY, matrixMontante[50][50];
+int potX[50], matrixQ;
 void setup(){
 
     for (int i=0; i<10; i++) {
@@ -58,9 +58,9 @@ void implementIntoMatrix(){
     int i=0;
     
     
-    for (int renglon=0; renglon<datos; renglon++) {
+    for (int renglon=0; renglon<=grado; renglon++) {
         
-        for (int colum=0; colum<datos; colum++) {
+        for (int colum=0; colum<=grado; colum++) {
             
             matrixSums[renglon][colum]=sumPotX[colum+i];
             
@@ -68,23 +68,37 @@ void implementIntoMatrix(){
         }
         i+=1;
     }
-
+    /*
     for (int i= 0; i<datos; i++) {
         matrixSums[i][datos]=sumPotxY[i];
     }
+     */
     
 }
 void desplegarMatrix(){
 
-    for (int i=0; i<datos; i++) {
-        for (int j=0; j<=datos; j++) {
+    for (int i=0; i<grado+1; i++) {
+        for (int j=0; j<=grado; j++) {
             cout<<matrixSums[i][j]<<" ";
+            //cout<<matrixMontante[i][j]<<" ";
         }
         cout<<endl;
     }
 
 }
+void findQ(){
 
+//tener una variable que defina el grado de cada ecuacion que estoy operando
+// osea una relacion entre unos valores de un renglon de matrixEcuations con lo que vendriera hacer su ecuacion
+
+//cada a, b, c, .. n-1,n serian representados como a + bx +cx^2...
+
+    //entonces seria un for que por cada renglon en matrixQ, entra en otro for que suma la funcion del renglon de matrixEcuations multiplicado o sumado, para luego ser restado por yi del mismo nivel de xi. Para así, finalmente ser elevado al cuadrado. esto se guarda en una matrizSol que es de los renglones necesarios pero una sola columna.
+    
+    //busco el dato menor.. ya esta.
+
+
+}
 
 int main() {
 	
@@ -92,7 +106,7 @@ int main() {
 	cout<<"Ingrese la cantidad de datos a ajustar: ";
 	cin>>datos;
 	cout<<"Ingrese los pares de numeros de [x,y]"<<endl;
-	
+	int n=datos;
 	for(int i=0; i<datos; i++) {
 		cin>>x[i];
         cin>>y[i];
@@ -102,11 +116,28 @@ int main() {
     // desplegar();
 	cout<<"Hasta que grado del polinomio quiere ajustar?: ";
 	cin>>grado;
+    int d=grado;
     
     Modelacion();
    
     implementIntoMatrix();
-    desplegarMatrix();
+ 
+    // while (n > 0) {
+        for (int r=0; r<n; r++) {
+            for (int c=0; c<n+2; c++) {
+                matrixMontante[r][c]= matrixSums[r][c];
+            }
+        }
+        
+        desplegarMatrix();
+        //Hago Montante aqui
+        //guardo ecuacion en una tercera matriz, llamada matrixEcuations
+        
+   //     n-=1;
+   // }
+    
+    findQ(); //Q=sum of r^2 = sum of all (f(x)-yi)^2
+    //hago Q
     
 	return 0;
 }
